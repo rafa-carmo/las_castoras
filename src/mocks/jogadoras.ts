5817e862157ddb9c256d536b2927dbeb7f3342d5
Pedro Vidinha <pedrofelippevidinha@gmail.com>
2023-12-12 00:19:27 -0300

    feat: mock names players

--- src/mocks/jogadoras.ts
+++ src/mocks/jogadoras.ts
@@ -2,8 +2,80 @@ import { Jogadora } from '@/components/TeamSlider'
 
 export const jogadorasMock: Jogadora[] = [
   {
+    name: 'Leila Aguirre',
+    image: '/assets/novos_elementos/Placas sem fundo/Arquera1.png',
+    assoc: 'ASO',
+  },
+
+  {
+    name: 'Delfina Molina',
+    image: '/assets/novos_elementos/Placas sem fundo/Arquera2.png',
+    assoc: 'ASLP',
+  },
+
+  {
+    name: 'Jacqueline Barrientos',
+    image: '/assets/novos_elementos/Placas sem fundo/1.png',
+    assoc: 'ASAM',
+  },
+
+  {
+    name: 'Camila Castaño',
+    image: '/assets/novos_elementos/Placas sem fundo/2.png',
+    assoc: 'ASLP',
+  },
+
+  {
+    name: 'Alejandra Farfán',
+    image: '/assets/novos_elementos/Placas sem fundo/3.png',
+    assoc: 'ASAM',
+  },
+
+  {
+    name: 'Nerina Staravijosky',
+    image: '/assets/novos_elementos/Placas sem fundo/4.png',
+    assoc: 'ASO',
+  },
+
+  {
+    name: 'Noelia Avila',
+    image: '/assets/novos_elementos/Placas sem fundo/5.png',
+    assoc: 'ASAM',
+  },
+
+  {
+    name: 'Noelia Rodríguez',
+    image: '/assets/novos_elementos/Placas sem fundo/6.png',
+    assoc: 'ASZNMA',
+  },
+
+  {
+    name: 'Sofía Rojas',
+    image: '/assets/novos_elementos/Placas sem fundo/7.png',
+    assoc: 'ASZNMA',
+  },
+
+  {
+    name: 'Andrea Esper',
+    image: '/assets/novos_elementos/Placas sem fundo/8.png',
+    assoc: 'ASM',
+  },
+
+  {
+    name: 'Lucía Rivas',
+    image: '/assets/novos_elementos/Placas sem fundo/9.png',
+    assoc: 'ASCS',
+  },
+
+  {
     name: 'Magali Carcagno',
     image: '/assets/novos_elementos/Placas sem fundo/10.png',
     assoc: 'ASO',
+  },
+
+  {
+    name: 'Camila Bustos',
+    image: '/assets/novos_elementos/Placas sem fundo/11.png',
+    assoc: 'ASCS',
   },
 ]
